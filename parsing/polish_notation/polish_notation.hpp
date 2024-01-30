24cee4f10ab7201c4d825f29beaf0e0a9b4ab6d7
VVabi <vabian.lenhardt@gmail.com>
2024-01-30 18:36:36 +0100

    Better structure

---
+++ parsing/polish_notation/polish_notation.hpp
@@ -0,0 +1,168 @@
+/*
+ * polish_notation.hpp
+ *
+ *  Created on: Jan 30, 2024
+ *      Author: vabi
+ */
+
+#ifndef PARSING_POLISH_NOTATION_POLISH_NOTATION_HPP_
+#define PARSING_POLISH_NOTATION_POLISH_NOTATION_HPP_
+
+#include <memory>
+#include <deque>
+#include "types/power_series.hpp"
+
+template<typename T>  class PolishNotationElement {
+ public:
+	virtual ~PolishNotationElement() { };
+	virtual FormalPowerSeries<T> handle_power_series(std::deque<std::unique_ptr<PolishNotationElement<T>>>& cmd_list,
+									const T unit,
+									const size_t fp_size) = 0;
+};
+
+template<typename T> FormalPowerSeries<T> iterate_polish(std::deque<std::unique_ptr<PolishNotationElement<T>>>& cmd_list,
+		const T unit,
+		const size_t fp_size) {
+	auto current = std::move(cmd_list.front());
+	cmd_list.pop_front();
+	return current->handle_power_series(cmd_list, unit, fp_size);
+}
+
+template<typename T> class PolishPlus : public PolishNotationElement<T> {
+	FormalPowerSeries<T> handle_power_series(std::deque<std::unique_ptr<PolishNotationElement<T>>>& cmd_list,
+										const T unit,
+										const size_t fp_size) {
+		auto left  = iterate_polish<T>(cmd_list, unit, fp_size);
+	    auto right = iterate_polish<T>(cmd_list, unit, fp_size);
+	    return left+right;
+	}
+};
+
+template<typename T>  class PolishMinus: public PolishNotationElement<T> {
+	FormalPowerSeries<T> handle_power_series(std::deque<std::unique_ptr<PolishNotationElement<T>>>& cmd_list,
+										const T unit,
+										const size_t fp_size) {
+	    auto left  = iterate_polish<T>(cmd_list, unit, fp_size);
+	    auto right = iterate_polish<T>(cmd_list, unit, fp_size);
+	    return left-right;
+	}
+};
+
+template<typename T>  class PolishTimes: public PolishNotationElement<T> {
+	FormalPowerSeries<T> handle_power_series(std::deque<std::unique_ptr<PolishNotationElement<T>>>& cmd_list,
+										const T unit,
+										const size_t fp_size) {
+	    auto left  = iterate_polish<T>(cmd_list, unit, fp_size);
+	    auto right = iterate_polish<T>(cmd_list, unit, fp_size);
+	    return left*right;
+	}
+};
+
+template<typename T>  class PolishDiv: public PolishNotationElement<T> {
+	FormalPowerSeries<T> handle_power_series(std::deque<std::unique_ptr<PolishNotationElement<T>>>& cmd_list,
+										const T unit,
+										const size_t fp_size) {
+	    auto left  = iterate_polish<T>(cmd_list, unit, fp_size);
+	    auto right = iterate_polish<T>(cmd_list, unit, fp_size);
+	    return left/right;
+	}
+};
+
+template<typename T>  class PolishVariable: public PolishNotationElement<T> {
+	FormalPowerSeries<T> handle_power_series(std::deque<std::unique_ptr<PolishNotationElement<T>>>& cmd_list,
+										const T unit,
+										const size_t fp_size) {
+		auto res = FormalPowerSeries<T>::get_atom(unit, 1, fp_size);
+		return res;
+	}
+};
+
+template<typename T> class PolishUnaryMinus: public PolishNotationElement<T> {
+	FormalPowerSeries<T> handle_power_series(std::deque<std::unique_ptr<PolishNotationElement<T>>>& cmd_list,
+										const T unit,
+										const size_t fp_size) {
+		auto result = iterate_polish<T>(cmd_list, unit, fp_size);
+		return -result;
+	}
+};
+
+
+template<typename T>  class PolishNumber: public PolishNotationElement<T> {
+private:
+	std::string num_repr;
+public:
+	PolishNumber(std::string num_repr): num_repr(num_repr) { }
+	FormalPowerSeries<T> handle_power_series(std::deque<std::unique_ptr<PolishNotationElement<T>>>& cmd_list,
+										const T unit,
+										const size_t fp_size) {
+		auto num = RingCompanionHelper<T>::from_string(num_repr, unit);
+		return FormalPowerSeries<T>::get_atom(num, 0, fp_size);
+	}
+};
+
+template<typename T>  class PolishPower: public PolishNotationElement<T> {
+ private:
+	uint32_t exponent;
+ public:
+	PolishPower(uint32_t exponent) {
+		this->exponent = exponent;
+	}
+
+	uint32_t get_exponent() {
+		return exponent;
+	}
+
+	FormalPowerSeries<T> handle_power_series(std::deque<std::unique_ptr<PolishNotationElement<T>>>& cmd_list,
+										const T unit,
+										const size_t fp_size) {
+		return FormalPowerSeries<T>::get_atom(unit, exponent, fp_size);
+	}
+};
+
+
+template<class RandomAccessIterator, typename T> std::deque<std::unique_ptr<PolishNotationElement<T>>>
+parse_polish_string(RandomAccessIterator begin,RandomAccessIterator end) {
+	auto ret = std::deque<std::unique_ptr<PolishNotationElement<T>>>();
+
+	for (auto it = begin; it != end; it++) {
+		auto next = *it;
+		if (next == "+") {
+			ret.push_back(std::move(std::make_unique<PolishPlus<T>>()));
+		} else if (next == "-") {
+			ret.push_back(std::move(std::make_unique<PolishMinus<T>>()));
+		} else if (next == "*") {
+			ret.push_back(std::move(std::make_unique<PolishTimes<T>>()));
+		} else if (next == "/") {
+			ret.push_back(std::move(std::make_unique<PolishDiv<T>>()));
+		}  else if (next == "#-") {
+			ret.push_back(std::move(std::make_unique<PolishUnaryMinus<T>>()));
+		}  else if (next == "z") {
+			ret.push_back(std::move(std::make_unique<PolishVariable<T>>()));
+		} else if (next == "^") {
+			auto var = *(++it);
+			auto exponent = stoi(*(++it));
+			assert(var == "z");
+			ret.push_back(std::move(std::make_unique<PolishPower<T>>(exponent)));
+		} else {
+			ret.push_back(std::move(std::make_unique<PolishNumber<T>>(*it)));
+		}
+	}
+	return ret;
+}
+
+template<class RandomAccessIterator, typename T> FormalPowerSeries<T>
+	convert_polish_notation_to_fp(RandomAccessIterator begin,
+								RandomAccessIterator end,
+								const uint32_t precision,
+								const T unit) {
+
+	auto parsed = parse_polish_string<RandomAccessIterator, T>(begin, end);
+	auto res = iterate_polish<T>(parsed, unit, precision);
+	return res;
+}
+
+
+
+
+
+#endif /* PARSING_POLISH_NOTATION_POLISH_NOTATION_HPP_ */
--- types/power_series.hpp
+++ types/power_series.hpp
@@ -11,6 +11,7 @@
 #include <vector>
 #include <iterator>
 #include <sstream>
+#include <string>
 #include "modLong.hpp"
 
 template<typename T> class RingCompanionHelper {
@@ -20,6 +21,10 @@ template<typename T> class RingCompanionHelper {
 	}
 	static T get_unit(const T& in) {
 		return 1;
+	}
+
+	static T from_string(const std::string& in, const T& unit) {
+		return stoi(in);
 	}
 };
 
@@ -31,9 +36,24 @@ public:
 	static ModLong get_unit(const ModLong& in) {
 		return ModLong(1, in.get_modulus());
 	}
+	static ModLong from_string(const std::string& in, const ModLong& unit) {
+		return ModLong(std::stoi(in), unit.get_modulus());
+	}
 };
 
+template<> class RingCompanionHelper<double> {
+ public:
+	static double get_zero(const double& in) {
+		return 0.0;
+	}
+	static double get_unit(const double& in) {
+		return 1.0;
+	}
 
+	static double from_string(const std::string& in, const double& unit) {
+		return stod(in);
+	}
+};
 
 
 template<typename T> class FormalPowerSeries {
