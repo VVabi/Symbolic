1c20f9abd62530de672b2a9e4150db634b390ea3
VVabi <vabian.lenhardt@gmail.com>
2024-07-27 15:04:48 +0200

    Reverted playground

--- src/Symbolic_playground.cpp
+++ src/Symbolic_playground.cpp
@@ -19,59 +19,7 @@
 #include "shell/parameters/parameters.hpp"
 
 
-/*class TestShell: public CoreShell {
-  public:
-    std::stringstream out;
-    std::stringstream err;
-    std::ifstream input_file;
-
-    std::vector<std::string> outputs;
-    std::vector<std::string> errs;
-
-    TestShell(): input_file("../test.txt") {  
-        out = std::stringstream();
-        err = std::stringstream();   
-    }
-
-    std::string get_next_input() {
-        std::string input;
-        if (std::getline(input_file, input)) {
-            return input;
-        }
-        return "exit";
-    }
-
-    void handle_output(std::unique_ptr<FormulaParsingResult> result, bool print_result) {
-        UNUSED(print_result);
-        result->print_result(out, err, print_result);
-        outputs.push_back(out.str());
-        errs.push_back(err.str());
-        out.str("");
-        err.str("");
-    }
-};*/
-
-bool test_shell_power_series_parsing();
-bool test_shell_explicit_tests();
-void regenerate_outputs();
 int main(int argc, char **argv) {
     UNUSED(argc);
     UNUSED(argv);
-
-    initialize_shell_parameters();
-    initialize_command_handler();
-    //regenerate_outputs();
-    test_shell_explicit_tests();
-    /*auto shell = std::make_shared<TestShell>();
-    SymbolicShellEvaluator evaluator(shell);
-    
-    evaluator.run();
-
-    for (auto& output: shell->outputs) {
-        std::cout << output << std::endl;
-    }
-    for (auto& err: shell->errs) {
-        std::cerr << err << std::endl;
-    }
-    return 0;*/
 }
