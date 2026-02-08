acbfd205dbc70d6ec5531d17b40594124515cda8
Vvabi <vabian.lenhardt@gmail.com>
2026-02-08 14:20:07 +0100

    Test fix + removed erroneous code

--- include/parsing/sym_object.hpp
+++ include/parsing/sym_object.hpp
@@ -8,14 +8,10 @@ class SymObject {
  public:
     virtual ~SymObject() = default;
     virtual std::string to_string() const = 0;
-    virtual std::shared_ptr<SymMathObject> as_math_object() const = 0;
 };
 
 
 class SymMathObject: public SymObject {
  public:
     virtual ~SymMathObject() = default;
-    std::shared_ptr<SymMathObject> as_math_object() const override {
-        return std::const_pointer_cast<SymMathObject>(std::shared_ptr<const SymMathObject>(this));
-    }
 };
